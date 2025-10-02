4a0cbb67179a5687063a2fd72376c23bb2eb383b
Adeel <adeel.m.zahid@gmail.com>
2025-10-02 16:26:02 -0500

    modified references in contribution label

--- src/mmwave/model/mmwave-spectrum-phy.cc
+++ src/mmwave/model/mmwave-spectrum-phy.cc
@@ -29,7 +29,7 @@
  *                 Dual Connectivity and Handover functionalities
  *
  * Modified by: Muhammad Adeel Zahid <zahidma@myumanitoba.ca>
- *                 Integrating NTNs & Multilayer support with IAB derived from ns3-mmwave-iab, ns3-ntn and ns3-mmwave-hbf
+ *                 Integrating NTNs & Multilayer support with IAB derived from signetlabdei/ns3-mmwave-iab, Mattia Sandri/ns3-ntn and signetlabdei/ns3-mmwave-hbf
  *                 
  */
 

235acc9cec374e9ef425a97b2b86abad8092468e
Adeel <adeel.m.zahid@gmail.com>
2025-10-10 15:54:03 -0500

    updated contribution tag

--- src/mmwave/model/mmwave-beamforming.cc
+++ src/mmwave/model/mmwave-beamforming.cc
@@ -26,7 +26,7 @@
  *			Dual Connectivity and Handover functionalities
  *
  * Modified by: Muhammad Adeel Zahid <zahidma@myumanitoba.ca>
- *                 Integrating NTNs & Multilayer support with IAB with MPEG-DASH video streaming with quic derived from signetlabdei/ns3-mmwave-iab, Mattia Sandri/ns3-ntn, signetlabdei/ns3-mmwave-hbf, signetlabdei/quic-ns-3 and ssjShirley/mpquic-ns3
+ *                 Integrating NTNs & Multilayer support with IAB derived from signetlabdei/ns3-mmwave-iab, Mattia Sandri/ns3-ntn and signetlabdei/ns3-mmwave-hbf
  *                 
  */
 
